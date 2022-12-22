f92dedb0be988e5d36f4513b9e7de01cfc5120bd
Begudem Srinath <92862452+srinath0230@users.noreply.github.com>
2022-12-22 12:16:21 +0530

    Update PROJECT 4 - Rock_Paper_Scissors.py

--- PROJECT 4 - Rock_Paper_Scissors.py
+++ PROJECT 4 - Rock_Paper_Scissors.py
@@ -28,31 +28,30 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+
+# created a list and optimised the problem instead of using if-else statement
+
+game_images = [rock,paper,scissors]
+
+# prompt user for input
 user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n"))
-if user_input == 0:
-  print(rock)
-elif user_input == 1:
-  print(paper)
-elif user_input == 2:
-  print(scissors)
-
-print("computer chose: " )
-computer_chose = random.randint(0,2)
-
-if computer_chose == 0:
-  print(rock)
-elif computer_chose == 1:
-  print(paper)
-elif computer_chose == 2:
-  print(scissors)
-
-if user_input == 0 and computer_chose == 1:
-  print("You Lose!")
-elif user_input == 1 and computer_chose == 2:
-  print("You Lose!")
-elif user_input == 2 and computer_chose == 0:
-  print("You Lose!")
-elif user_input == computer_chose:
-  print("Draw!")
+# checks the conidition if the user enter valid number
+if user_input >= 0 and user_input <= 2:  
+  print(game_images[user_input])
+# computer chose is random
+  computer_chose = random.randint(0,2)
+  print(f"computer chose: {game_images[computer_chose]}") 
+# checks the rules of the game and declear winner
+  if user_input == 0 and computer_chose == 1:
+    print("You Lose!")
+  elif user_input == 1 and computer_chose == 2:
+    print("You Lose!")
+  elif user_input == 2 and computer_chose == 0:
+    print("You Lose!")
+  elif user_input == computer_chose:
+    print("Draw!")
+  else:
+    print("You Win!")
+# if condition fails / invaild input 
 else:
-  print("You Win!")
+  print("Invalid number,You Lose!")
