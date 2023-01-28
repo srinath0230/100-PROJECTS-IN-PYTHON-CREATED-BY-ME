326591e64f612c258fb8b1f5580789c9fab46324
Begudem Srinath <92862452+srinath0230@users.noreply.github.com>
2023-01-28 22:48:09 +0530

    Create BMI_Calculator.py

---
+++ Coding Exercises/BMI_Calculator.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+i=int(weight)
+j=float(height)
+BMI = i/j**2
+
+print(round(BMI))
